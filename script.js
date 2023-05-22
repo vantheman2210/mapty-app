3bcb49a33e0d091a943873590c336df41a3cbc1f
vantheman2210 <martinovic.ba@gmail.com>
2023-05-22 10:49:00 +0200

    Implementing leaflet library to the project

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ const inputElevation = document.querySelector(".form__input--elevation");
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
-      
-      const {latitude, longitude} = position.coords;
-      console.log(latitude, longitude)
+      const { latitude, longitude } = position.coords;
+      console.log(latitude, longitude);
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
     },
     function () {
       alert("Could not get your position");
