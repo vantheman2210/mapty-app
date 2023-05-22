3d7e57eaae2d03cd784ca971529e4f1fc8d94744
vantheman2210 <martinovic.ba@gmail.com>
2023-05-22 11:02:06 +0200

    Leaflet uses geolocation coordinated to show map

--- script.js
+++ script.js
@@ -17,7 +17,20 @@ if (navigator.geolocation)
     function (position) {
       const { latitude, longitude } = position.coords;
       console.log(latitude, longitude);
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+      const coords = [latitude, longitude]
+
+      const map = L.map("map").setView(coords, 13);
+
+      L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+        .openPopup();
     },
     function () {
       alert("Could not get your position");
