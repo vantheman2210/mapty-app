77959c210913e359129e1cace5fd40758806c703
vantheman2210 <martinovic.ba@gmail.com>
2023-05-22 10:38:07 +0200

    Adding geolocation API

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+
+// Obtaining user geo location
+navigator.geolocation.getCurrentPosition(function(position) { 
+  console.group(position);
+}, function() { 
+  alert('Could not get your position')
+})
