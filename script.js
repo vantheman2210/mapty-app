a7825561b406d8074f5138ef3e34f1357da421c5
vantheman2210 <martinovic.ba@gmail.com>
2023-05-22 13:04:38 +0200

    Form submit shows map marker

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+let map;
+
 // Obtaining user geo location
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
@@ -20,7 +22,7 @@ if (navigator.geolocation)
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
       const coords = [latitude, longitude];
 
-      const map = L.map("map").setView(coords, 13);
+      map = L.map("map").setView(coords, 13);
 
       L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
         attribution:
@@ -28,7 +30,17 @@ if (navigator.geolocation)
       }).addTo(map);
 
       map.on("click", function (mapEvent) {
-        console.log(mapEvent);
+        form.classList.remove('hidden')
+        inputDistance.focus();
+      });
+    },
+    function () {
+      alert("Could not get your position");
+    }
+  );
+
+  form.addEventListener('submit', function () { 
+    console.log(mapEvent);
         const { lat, lng } = mapEvent.latlng;
         L.marker([lat, lng])
           .addTo(map)
@@ -42,9 +54,9 @@ if (navigator.geolocation)
           }))
           .setPopupContent('Workout')
           .openPopup();
-      });
-    },
-    function () {
-      alert("Could not get your position");
-    }
-  );
+  })
+
+
+  /*
+    
+  */
