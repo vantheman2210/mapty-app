02d4db8148ce751b308fda4a00aefcb52a93cc36
vantheman2210 <martinovic.ba@gmail.com>
2023-05-23 11:59:23 +0200

    adding method for rendering marker on map

--- script.js
+++ script.js
@@ -58,6 +58,7 @@ class Cycling extends Workout {
 class App {
   #map;
   #mapEvent;
+  #workouts = [];
 
   constructor() {
     this._getPosition();
@@ -108,33 +109,49 @@ class App {
 
   _newWorkout(e) {
     const validInputs = (...inputs) =>
-      inputs.every((input) => Number.isFinite(input) && Number(input) > 0);
+      inputs.every((input) => Number.isFinite(input));
     e.preventDefault();
+    const allPositive = (...inputs) =>
+      inputs.every((input) => Number(input) > 0);
 
     // Get data from the form
     const type = inputType.value;
     const distance = +inputDistance.value;
     const duration = +inputDuration.value;
+    const { lat, lng } = this.#mapEvent.latlng;
+    let workout;
 
     // if it is running, create running object
     if (type === "running") {
       const cadence = +inputCadence.value;
       // Check if data is valid
-      if (!validInputs(distance, duration, cadence))
+      if (
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      )
         return alert("Inputs have to be positive numbers");
+      workout = new Running([lat, lng], distance, duration, cadence);
     }
 
     // if cycling, create cycling object
     if (type === "cycling") {
       const elevation = +inputElevation.value;
-      if (!validInputs(distance, duration, elevation))
+      if (
+        !validInputs(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      )
         return alert("Inputs have to be positive numbers");
+      workout = new Cycling([lat, lng], distance, duration, elevation);
     }
 
     // add new object to workout array
+    this.#workouts.push(workout);
+    console.log(workout);
+  }
 
+  renderWorkoutMarker() {
     // Render workout on map as marker
-    const { lat, lng } = this.#mapEvent.latlng;
+
     L.marker([lat, lng])
       .addTo(this.#map)
       .bindPopup(
@@ -143,7 +160,7 @@ class App {
           minWidth: 100,
           autoClose: false,
           closeOnClick: false,
-          className: "running-popup",
+          className: `${type}-popup`,
         })
       )
       .setPopupContent("Workout")
